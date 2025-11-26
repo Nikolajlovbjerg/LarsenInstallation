e24cd59bb3e76be19fd361627291cffbe4f1c5e4
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2025-11-26 09:17:27 +0100

    asdasdasd

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,13 @@
+using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp =>
-    new HttpClient { BaseAddress = new Uri("http://localhost:5028/") });
+    new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
