1b01a4bdbdc6b2f2f0ce9153838c0023055e165e
MortenHHD <eaa24mhhda@students.eaaa.dk>
2025-12-03 11:37:28 +0100

    flere ændringer i mock
    1

--- Server/Repositories/Project/ProjectMock.cs
+++ Server/Repositories/Project/ProjectMock.cs
@@ -11,7 +11,7 @@ public class ProjectRepositoryMock : IProjectRepo
         {
             ProjectId = 1,
             Name = "Elinstallation i villa",
-            DateCreated = DateTime.Now.AddDays(-7),
+            DateCreated = DateTime.Now.ToLocalTime(),
             SvendTimePris = 500,
             LærlingTimePris = 200,
             KonsulentTimePris = 800,
@@ -48,7 +48,7 @@ public class ProjectRepositoryMock : IProjectRepo
         {
             ProjectId = 2,
             Name = " JumboLombo villa",
-            DateCreated = DateTime.Now.AddDays(-7),
+            DateCreated = DateTime.Now,
             SvendTimePris = 500,
             LærlingTimePris = 200,
             KonsulentTimePris = 800,
@@ -58,6 +58,80 @@ public class ProjectRepositoryMock : IProjectRepo
                 new House
                 {
                     Name = "Hus 2",
+                    CostCustomer = 20000,
+                    CostCompany = 14000,
+                    Hours = 80,
+                    InstallationCosts = new Dictionary<string, decimal>
+                    {
+                        { "Stikkontakter", 1000 },
+                        { "Belysning", 2000 }
+                    },
+                    MaterialsUsed = new Dictionary<string, int>
+                    {
+                        { "Ledninger", 50 },
+                        { "Stik", 20 }
+                    }
+                }
+            },
+            TotalCostCustomer = 50000,
+            TotalCostCompany = 35000,
+            TimeEntries = new List<TimeEntry>
+            {
+                new TimeEntry { Date = DateTime.Today.AddDays(-5), Hours = 5 },
+                new TimeEntry { Date = DateTime.Today.AddDays(-4), Hours = 6 }
+            }
+        });
+        mProjects.Add(new Project
+        {
+            ProjectId = 3,
+            Name = " JumboLomboCombo villa",
+            DateCreated = DateTime.Now,
+            SvendTimePris = 500,
+            LærlingTimePris = 200,
+            KonsulentTimePris = 800,
+            ArbejdsmandTimePris = 250,
+            Houses = new List<House>
+            {
+                new House
+                {
+                    Name = "Hus 3",
+                    CostCustomer = 20000,
+                    CostCompany = 14000,
+                    Hours = 80,
+                    InstallationCosts = new Dictionary<string, decimal>
+                    {
+                        { "Stikkontakter", 1000 },
+                        { "Belysning", 2000 }
+                    },
+                    MaterialsUsed = new Dictionary<string, int>
+                    {
+                        { "Ledninger", 50 },
+                        { "Stik", 20 }
+                    }
+                }
+            },
+            TotalCostCustomer = 50000,
+            TotalCostCompany = 35000,
+            TimeEntries = new List<TimeEntry>
+            {
+                new TimeEntry { Date = DateTime.Today.AddDays(-5), Hours = 5 },
+                new TimeEntry { Date = DateTime.Today.AddDays(-4), Hours = 6 }
+            }
+        });
+        mProjects.Add(new Project
+        {
+            ProjectId = 4,
+            Name = " JumboLomboComboWombo villa",
+            DateCreated = DateTime.Now,
+            SvendTimePris = 500,
+            LærlingTimePris = 200,
+            KonsulentTimePris = 800,
+            ArbejdsmandTimePris = 250,
+            Houses = new List<House>
+            {
+                new House
+                {
+                    Name = "Hus 4",
                     CostCustomer = 20000,
                     CostCompany = 14000,
                     Hours = 80,
