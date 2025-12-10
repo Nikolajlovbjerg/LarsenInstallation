e9e2c1ccd0798fcc58d604cc7c665bd8248532b6
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2025-12-10 14:32:11 +0100

    comments

--- Server/Repositories/User/ICreateUserRepoSQL.cs
+++ Server/Repositories/User/ICreateUserRepoSQL.cs
@@ -1,12 +1,14 @@
 ﻿using Core;
-using System.Collections.Generic;
+
 namespace Server.Repositories.User
 {
+    // Interface som beskriver hvilke metoder et "User Repository" skal have
+    // Formål er at være en mellem mand imellem repo og controller
     public interface ICreateUserRepoSQL
     {
-        List<Users> GetAll();
-        void Add(Users user);
-        void Delete(int id);
-        Users? ValidateUser(string username, string password);
+        List<Users> GetAll();   // Skal kunne hente alle brugere
+        void Add(Users user);   // Skal kunne tilføje en ny bruger
+        void Delete(int id);    // Skal kunne slette en bruger ud fra ID
+        Users? ValidateUser(string username, string password);  // Skal kunne validere login
     }
 }
